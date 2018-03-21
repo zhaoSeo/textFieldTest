2b3ad6ff5db101cdff4baae7c2ebafdcfd206985
서상원 <zhao@192.168.nate.com>
2018-03-21 15:34:29 +0900

    delegate

--- textFieldTest/ViewController.swift
+++ textFieldTest/ViewController.swift
@@ -8,18 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var myTextField: UITextField!
+    @IBOutlet weak var myLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        myTextField.delegate = self // delegate connection
+        myTextField.clearButtonMode = UITextFieldViewMode.always
+        myTextField.placeholder = "입력하시오!"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    @IBAction func btnPressed(_ sender: UIButton) {
+        myLabel.text = myTextField.text
+        myTextField.text = ""
+        myTextField.resignFirstResponder()
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        myTextField.resignFirstResponder() //keypad down
+        return true
+    }
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        view.backgroundColor = UIColor.yellow
+        return true
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
 }
 
